98b98119fbc692c04cd7af038b3349678ebd533b
fridmaster <fridmaster@gmail.com>
2020-08-08 16:23:50 +0300

    first nice version

--- src/router.ts
+++ src/router.ts
@@ -2,75 +2,61 @@ import { Express, Router } from 'express';
 import * as path from 'path';
 
 export default class AppRouter {
-    public router;
-    private app;
-    private subscribers = {};
-    constructor(app: Express) {
-        this.router = Router();
-        this.app = app;
+  public router;
+  private app;
+  private subscribers = {};
+  constructor(app: Express) {
+    this.router = Router();
+    this.app = app;
+  }
 
-    }
+  public init(): void {
 
-    public init(): void {
-        this.router.get('/', (req, res) => {
-            console.log('we got it here', req.body);
-            res.send('WE SEND IT BACk')
-        });
-        this.router.post('/publish', (req, res)=>{
-             let after = req.query.after;
-            after = 3000;
-            // принять POST-запрос
-            req.setEncoding('utf8');
-            var message = '';
-            req.on('data', function(chunk) {
-            message += chunk;
-            }).on('end', () => {
-              this.publish(message, after, res); // собственно, отправка
-            });
+    this.router.get('/publish', (req, res) => {
+      let after = req.query.after;
+      req.setEncoding('utf8');
+      var message = '';
+      req.on('data', function (chunk) {
+        message += chunk;
+      }).on('end', () => { 
+        this.publish(message, after, res); // собственно, отправка
+      });
+      return;
+    });
+    this.router.get('/subscribe', (req, res) => {
+      this.onSubscribe(req, res);
+      return;
+    })
 
-            return;
-        });
-        this.router.get('/subscribe', (req, res)=>{
-            this.onSubscribe(req, res);
-            return;
-        })
+    this.router.get('/', (_req, res) => {
+      res.sendFile(path.resolve(__dirname + '/index.html'));
+    })
+    this.app.use('/', this.router);
+  }
 
-        this.router.get('/chat', (_req, res)=> {
+  private onSubscribe(req, res) {
+    const id = Math.random();
+    res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+    res.setHeader("Cache-Control", "no-cache, must-revalidate");
 
-          console.log(path.join(__dirname + '/index.html'));
-          res.sendFile(path.resolve(__dirname + '/index.html'));
-          // next();
-        })
-        this.app.use('/', this.router);
-    }
+    this.subscribers[id] = res;
+    req.on('close', () => {
+      delete this.subscribers[id];
+    });
 
-    private onSubscribe(req, res) {
-        const id = Math.random();
-        res.setHeader('Content-Type', 'text/plain;charset=utf-8');
-        res.setHeader("Cache-Control", "no-cache, must-revalidate");
+  }
+
+  private publish(message, timeout, resOnPub) {
+    resOnPub.send(JSON.stringify({ message: `timeout will be ${timeout}` }));
+    for (var id in this.subscribers) {
+      const res = this.subscribers[id];
+      setTimeout(() => {
+
+        res.end(message);
         
-        this.subscribers[id] = res;
-        //console.log("новый клиент " + id + ", клиентов:" + Object.keys(subscribers).length);
-        req.on('close', () => {
-          delete this.subscribers[id];
-          //console.log("клиент "+id+" отсоединился, клиентов:" + Object.keys(subscribers).length);
-        });
-      
-      }
+      }, timeout);
+    }
 
-      private publish(message, timeout, resOnPub) {
-
-        //console.log("есть сообщение, клиентов:" + Object.keys(subscribers).length);
-        resOnPub.send(JSON.stringify({message:`timeout will be ${timeout}`}));
-        for (var id in this.subscribers) {
-          //console.log("отсылаю сообщение " + id);
-          const res = this.subscribers[id];
-          setTimeout(()=>{
-            res.end(message);
-            res
-          },timeout);
-        }
-      
-        this.subscribers = {};
-      }  
+    this.subscribers = {};
+  }
 }
