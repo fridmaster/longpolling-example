cc9dba36349370e95e912706fdd8bd1af2599f5f
lmn451 <cia_agent1@mail.ru>
2020-08-10 23:26:38 +0300

    class syntax added

--- src/polling-server.ts
+++ src/polling-server.ts
@@ -9,7 +9,7 @@ const expressRoutes = new AppRouter(app);
 expressRoutes.init();
 const port = process.env.PORT;
 app.use(express.static("src")) // serve the whole directory
-
+process.env.PORT = "3000"
 app.listen(process.env.PORT, () => {
   console.log(`Express server app listening on port ${process.env.PORT}!`);
 });
